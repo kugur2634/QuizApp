bf10bd93ba6ec6739419d5d2bcf00b422c79ee71
kugur3426 <kugur3426@hotmail.com>
2024-04-10 21:57:48 +0200

    base

---
+++ Quiz/questions.js
@@ -0,0 +1,113 @@
+// creating an array and passing the number, questions and answers
+let questions = [
+    {
+        numb: 1,
+        question: "2 x 2 = ?",
+        answer: "4",
+        options: [
+            "3",
+            "4",
+            "8",
+            "2"
+        ]
+    },
+    {
+        numb: 2,
+        question: "4 ^ 3 = ?",
+        answer: "64",
+        options: [
+            "12",
+            "16",
+            "64",
+            "128"
+        ]
+    },
+    {
+        numb: 3,
+        question: "(16 + 8) x 2 = ?",
+        answer: "48",
+        options: [
+            "48",
+            "46",
+            "44",
+            "42"
+        ]
+    },
+    {
+        numb: 4,
+        question: "1 x 1 = ?",
+        answer: "1",
+        options: [
+            "2",
+            "1.5",
+            "0",
+            "1"
+        ]
+    },
+    {
+        numb: 5,
+        question: "90 - 62 + 14 = ?",
+        answer: "42",
+        options: [
+            "47",
+            "42",
+            "45",
+            "52"
+        ]
+    },
+    {
+        numb: 6,
+        question: "77 / 11 = ?",
+        answer: "7",
+        options: [
+            "7",
+            "10",
+            "6",
+            "7.5"
+        ]
+    },
+    {
+        numb: 7,
+        question: "2 x 2 x 3 / 3 = ?",
+        answer: "4",
+        options: [
+            "2",
+            "3",
+            "4",
+            "6"
+        ]
+    },
+    {
+        numb: 8,
+        question: "log5 ^ 25 = ?",
+        answer: "2",
+        options: [
+            "1",
+            "3",
+            "5",
+            "2"
+        ]
+    },
+    {
+        numb: 9,
+        question: "f(x) = x + 7 - 3 = 40 , x = ?",
+        answer: "36",
+        options: [
+            "36",
+            "46",
+            "56",
+            "40"
+        ]
+    },
+    {
+        numb: 10,
+        question: "7 x 7 = ?",
+        answer: "49",
+        options: [
+            "47",
+            "49",
+            "48",
+            "46"
+        ]
+    }
+]
